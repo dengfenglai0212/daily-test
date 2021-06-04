57116afde1bb80309f59e960d0410a35029017e1
shuailiang <shuailiang@tencent.com>
2021-06-04 11:28:05 +0800

    feat: 增加流测试页面

--- test_03.js
+++ test_03.js
@@ -25,4 +25,15 @@
 // setFengtuo(aSb)
 // console.log(aSb)
 
-
+let obj ={
+  name:1,
+  age:2
+}
+console.log(obj)
+// for(var key in obj){
+//   console.log(key);
+//   obj[key] = null
+// }
+console.log(Object.keys(obj))
+Object.keys(obj).forEach(key=>{obj[key]=''})
+console.log(obj)
